2c063c0d65ab9d7d224a2f4620acc3aa39fd6b65
Vishwaa M S <vishwaams.03@gmail.com>
2024-01-26 15:16:29 +0530

    modified Chatpage.js

--- frontend/src/pages/Chatpage.js
+++ frontend/src/pages/Chatpage.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Chatpage = () => {
-  return <div>CHATPAGE</div>;
+  const [chats, setChats] = useState([]);
+  const fetchChats = async () => {
+    const { data } = await axios({
+      method: "get",
+      url: "http://127.0.0.1:5000/api/chat",
+      responseType: "json",
+    });
+    setChats(data);
+  };
+
+  useEffect(() => {
+    fetchChats();
+  }, []);
+
+  return (
+    <div>
+      {chats.map((chat) => (
+        <div key={chat._id}>{chat.chatName}</div>
+      ))}
+    </div>
+  );
 };
 
 export default Chatpage;
