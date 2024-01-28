405279488153eb83c951e312b6b9a388c740ae99
Vishwaa M S <vishwaams.03@gmail.com>
2024-01-28 23:44:56 +0530

    modified signup.js

--- frontend/src/components/Authentication/signup.js
+++ frontend/src/components/Authentication/signup.js
@@ -6,8 +6,11 @@ import {
   InputGroup,
   InputRightElement,
   Button,
+  useToast,
 } from "@chakra-ui/react";
+import axios from "axios";
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Signup = () => {
   const [show, setShow] = useState(false);
@@ -16,13 +19,132 @@ const Signup = () => {
   const [password, setPassword] = useState();
   const [confirmPassword, setConfirmPassword] = useState();
   const [pic, setPic] = useState();
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
+  const history = useHistory();
 
   const handleClick = () => {
     setShow(!show);
   };
 
-  const postDetails = (pics) => {};
-  const submitHandler = () => {};
+  const postDetails = (pic) => {
+    setLoading(true);
+    if (pic === undefined) {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+
+    if (
+      pic.type === "image/jpeg" ||
+      pic.type === "image/jpg" ||
+      pic.type === "image/png"
+    ) {
+      const data = new FormData();
+      data.append("file", pic);
+      data.append("upload_preset", "YoChat");
+      data.append("cloud_name", "ddzkfvyti");
+      fetch("https://api.cloudinary.com/v1_1/ddzkfvyti/image/upload", {
+        method: "POST",
+        body: data,
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setPic(data.url.toString());
+          console.log(data.url.toString());
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setLoading(false);
+        });
+    } else {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+  };
+  const submitHandler = async () => {
+    setLoading(true);
+    if (!name || !email || !password || !confirmPassword) {
+      toast({
+        title: "Please fill all the Fields!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      toast({
+        title: "Passwords Do Not Match!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const { data } = await axios({
+        method: "post",
+        url: "http://127.0.0.1:5000/api/user/signup",
+        data: {
+          name: name,
+          email: email,
+          password: password,
+          pic: pic,
+        },
+        responseType: "json",
+      });
+      toast({
+        title: "Registration Successful!",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      history.push("/chats");
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Error Occured!",
+
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+  };
   return (
     <VStack spacing={"5px"} color={"black"}>
       <FormControl id="name" isRequired>
@@ -77,7 +199,9 @@ const Signup = () => {
           type="file"
           p={1.5}
           accept="image/*"
-          onChange={(e) => postDetails(e.target.files[0])}
+          onChange={(e) => {
+            postDetails(e.target.files[0]);
+          }}
         />
       </FormControl>
 
@@ -86,6 +210,7 @@ const Signup = () => {
         width={"100%"}
         style={{ marginTop: 15 }}
         onClick={submitHandler}
+        isLoading={loading}
       >
         Sign Up
       </Button>
