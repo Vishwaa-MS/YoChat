27d0be05e1cab0cc0ca2a53bdee71cd40c1d04ad
Vishwaa M S <vishwaams.03@gmail.com>
2024-01-28 23:42:31 +0530

    addedd CORS access

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,16 @@ connectDB();
 
 app.use(bodyParser.json()); // application/json
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("App is working");
 });
