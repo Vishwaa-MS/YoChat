e9d8dd320c50915fb4f58a29574d3580341581ac
Vishwaa M S <vishwaams.03@gmail.com>
2024-01-28 23:44:38 +0530

    modified login.js

--- frontend/src/components/Authentication/login.js
+++ frontend/src/components/Authentication/login.js
@@ -8,16 +8,74 @@ import {
   Button,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { useToast } from "@chakra-ui/react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
+  const toast = useToast();
   const [show, setShow] = useState(false);
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
   const handleClick = () => {
     setShow(!show);
   };
-  const submitHandler = () => {};
+  const submitHandler = async () => {
+    setLoading(true);
+    if (!email || !password) {
+      toast({
+        title: "Please Fill all the Feilds",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const { data } = await axios({
+        method: "post",
+        url: "http://127.0.0.1:5000/api/user/login",
+        data: {
+          email: email,
+          password: password,
+        },
+        responseType: "json",
+      });
+
+      toast({
+        title: "Login Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      history.push("/chats");
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+  };
   return (
     <VStack spacing={"5px"} color={"black"}>
       <FormControl id="email" isRequired>
